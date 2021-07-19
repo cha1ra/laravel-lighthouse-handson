7eb10ca5e2b012ab9faa09b014bf82f6ecc329ba
cha1ra <cha1ra.garnet@gmail.com>
2021-07-19 14:09:23 +0900

    feat: jetstreamセットアップ

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name' => 'ハンズオン用ユーザ',
+            'email' => 'test@example.com'
+        ]);
+        User::factory(10)->create();
+    }
+}
