4205686a3183dfe6c6bea29b8203fd70bfc05459
cha1ra <cha1ra.garnet@gmail.com>
2021-07-20 22:18:46 +0900

    setup vue-apollo

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,30 @@ import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
 const el = document.getElementById('app');
 
 createApp({
+    setup () {
+      provide(DefaultApolloClient, apolloClient)
+    },
     render: () =>
         h(InertiaApp, {
             initialPage: JSON.parse(el.dataset.page),
