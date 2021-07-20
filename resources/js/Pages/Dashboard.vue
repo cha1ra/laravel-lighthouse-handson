1fdd909fd2e82bde41438d11437948ef88dd76fb
cha1ra <cha1ra.garnet@gmail.com>
2021-07-20 22:28:19 +0900

    template view

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,14 +2,14 @@
     <app-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+                Todo 一覧表示
             </h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
+                  ここに一覧を表示したい
                 </div>
             </div>
         </div>
@@ -17,13 +17,11 @@
 </template>
 
 <script>
-    import AppLayout from '@/Layouts/AppLayout'
-    import Welcome from '@/Jetstream/Welcome'
+import AppLayout from '@/Layouts/AppLayout'
 
-    export default {
-        components: {
-            AppLayout,
-            Welcome,
-        },
-    }
+export default {
+  components: {
+    AppLayout
+  }
+}
 </script>
